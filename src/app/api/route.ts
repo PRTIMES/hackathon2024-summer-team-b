37a0cc64e8fb75a373cd2acb57845db6649af3d5
Tomu Maekawa <tomu.m1107@icloud.com>
2024-08-28 18:52:01 +0900

    fix-types

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+import type { Keyword, Release } from "~/libs/types/prtimes";
+
 export async function GET(request: Request) {
   const res =  await fetch("https://hackathon.stg-prtimes.net/api/releases", {
     method: "GET",
@@ -7,9 +9,9 @@ export async function GET(request: Request) {
     }
     })
 
-  const data = await res.json();
+  const data = await res.json() as Release[];
   const articleData = await Promise.all(
-    data.map(async (e: any) => {
+    data.map(async (e) => {
       // 各データに基づいて新しいリクエストを送信
       const categoryRes = await fetch(`https://hackathon.stg-prtimes.net/api/companies/${e.company_id}/releases/${e.release_id}/keywords`, {
         method: "GET",
@@ -18,9 +20,9 @@ export async function GET(request: Request) {
           'Authorization': 'Bearer 37aaaf2e5398eec3521ca0408f9e0817999d81e014c000a3e65b55e6a807060c' // 必要に応じて認証トークンを置き換えてください
         }
       });
-      const {} = e
-      const data = await categoryRes.json()
-      const margeData = Object.assign(e, data)
+
+      const data = await categoryRes.json() as Keyword[]
+      const margeData = Object.assign(e, {category: data})
       return margeData; // 各レスポンスをJSONに変換して返す
     })
   );
--- src/libs/types/prtimes.ts
+++ src/libs/types/prtimes.ts
@@ -1,58 +1,25 @@
-interface AllArticleType {
-  body: string
+export interface Release {
+  company_name: string;
   company_id: number;
-  company_name: string,
-  created_at: string,
-  lead_paragraph: string,
-  like: number,
-  main_category_id: number,
-  main_category_name: string,
-  main_image: string,
-  main_image_fastly: string,
-  release_id: number,
-  release_type: string,
-  sub_category_id: 86,
-  sub_category_name: string,
-  subtitle: string,
-  title: string,
-  url: string,
-  }
-  
-  interface CategorType {
-    id: number,
-    name: string,
-    sort_priority: number;
-  }
-  
-interface ShowArticleType {
-  company_id: number,
-  company_name: string,
-  created_at: string,
-  main_category_name: string,
-  main_image: string,
-  release_id: number,
-  title: string,
+  release_id: number;
+  title: string;
+  subtitle: string;
+  url: string;
+  lead_paragraph: string;
+  body: string;
+  main_image: string;
+  main_image_fastly: string;
+  main_category_id: number;
+  main_category_name: string;
+  sub_category_id: number;
+  sub_category_name: string;
+  release_type: string;
+  created_at: string;
+  like: number;
 }
 
-interface MergeArticleDataType {
-  [key: string]: {
-    
-  },
-  body: string,
-  company_id: number,
-  company_name: string,
-  created_at: string,
-  lead_paragraph: string,
-  like: number,
-  main_category_id: number,
-  main_category_name: string,
-  main_image: string,
-  main_image_fastly: string,
-  release_id: number,
-  release_type: string,
-  sub_category_id: 86,
-  sub_category_name: string,
-  subtitle: string,
-  title: string,
-  url: string,
+export interface Keyword {
+  id: number;
+  name: string;
+  sort_priority: number;
 }
