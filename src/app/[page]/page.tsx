0ba30a4a445370e392bb3119950864e8883e4c7f
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-29 16:15:21 +0900

    feat: add search logic

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -5,14 +5,20 @@ import { ReleaseListPagination } from "../components/pagination";
 import ReleaseList from "../components/release-list";
 import { SelectDemo } from "../components/select-list/select-list";
 
-export default function Home({ params }: { params: { page: string } }) {
+export default function Home({
+  params,
+  searchParams,
+}: {
+  params: { page: string };
+  searchParams: { search: string };
+}) {
   return (
     <article>
       <Header />
       <div className="pt-14 sm:pt-20 pb-16 sm:pb-3">
         <ReleaseListPagination page={Number(params.page)} />
         <SelectDemo />
-        <ReleaseList page={Number(params.page)} />
+        <ReleaseList page={Number(params.page)} search={searchParams.search} />
         <SearchDrawer />
         <ReleaseListPagination page={Number(params.page)} />
       </div>
--- src/app/components/release-list/index.tsx
+++ src/app/components/release-list/index.tsx
@@ -5,14 +5,20 @@ import ReleaseItem from "./release-item";
 export default async function ReleaseList({
   page,
   category,
+  search,
 }: {
   page?: number;
   category?: string;
+  search?: string;
 }) {
   console.log("page", page);
   console.log("category", category);
+  console.log("search", search);
 
-  const releases = await getReleases({ offset: Number(page) * 12 - 12 });
+  const releases = await getReleases({
+    offset: Number(page) * 12 - 12,
+    search,
+  });
 
   if (!releases.success || !releases.data) {
     return (
@@ -29,7 +35,7 @@ export default async function ReleaseList({
 
   return (
     <main className="py-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-4 gap-3">
-      {releases.data!.map((release) => {
+      {releases.data!.map((release: any) => {
         return <ReleaseItem key={release.id} {...release} />;
       })}
     </main>
