20c77cd17e1dc0dad753e7390d6287bc337b3b00
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-29 13:32:13 +0900

    feat: add fetch releases

---
+++ src/app/data/releases.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { createClient } from "~/libs/supabase/server";
+
+export const getReleases = async ({ offset = 0, limit = 12 } = {}) => {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("releases")
+    .select(
+      `
+          *,
+          keywords:keyword_map(
+            keyword:keywords(name)
+          )
+        `,
+    )
+    .order("created_at", { ascending: false })
+    .range(offset, offset + limit - 1);
+
+  if (error) {
+    console.error("Error fetching releases:", error);
+    return { success: false, error: error.message };
+  }
+
+  return { success: true, data };
+};
