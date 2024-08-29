08385fa064f505324a21166e24ad7542e8f0e921
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-29 17:22:47 +0900

    refactor: link

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
+
 import SearchForm from "~/components/search/search-form";
-import { ScrollToTopButton } from "~/components/ui/scrollToTopButton";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center shadow bg-accent/50 backdrop-blur py-2 px-4 text-primary-foreground text-sm fixed top-0 z-50 w-full">
       <div className="text-center font-bold">
-        <a href="#top">
+        <Link href="/1">
           <h1 className="text-sm sm:text-sm">PR Carriers 🍓</h1>
-        </a>
+        </Link>
       </div>
       <div className="hidden sm:block">
         <SearchForm />
